8bd9dc8ced03acb2aecd3c4475a6c11d7e2e96bb
Lorenzo Pratissoli <lorenzo.pratissoli@hotmail.com>
2021-01-07 20:48:38 +0100

    Optimized vision/augment.py

--- autoagent/data/vision/augment.py
+++ autoagent/data/vision/augment.py
@@ -6,11 +6,12 @@ class Transform:
     def __init__(self):
         pass
 
-    def __call__(self):
+    def __call__(self, img):
         """
         Returns:
             - augmented image
-            - mapping func from old x,y to new x,y
+            - mapping func from old x,y pixel position
+              to new x,y position
         """
         raise NotImplementedError()
 
@@ -18,9 +19,9 @@ class Transform:
         return self.__class__.__name__
 
 
-class Identity:
-    def __call__(self, x):
-        return x, lambda x, y: (x, y)
+class Identity(Transform):
+    def __call__(self, img):
+        return img, lambda x, y: (x, y)
 
 
 class Resize(Transform):
@@ -34,8 +35,8 @@ class Resize(Transform):
         self.pad = pad
         self.fill_color = np.array([127, 127, 127], dtype=np.uint8)
 
-    def __call__(self, x):
-        h, w, _ = x.shape
+    def __call__(self, img):
+        h, w, _ = img.shape
 
         # Find target ratio
         ratio = min(self.size[1]/w, self.size[0]/h)
@@ -60,17 +61,17 @@ class Resize(Transform):
                 r = (self.size[0] - t_h)//2
                 d_h1, d_h2 = r, r+1
 
-            x = cv2.resize(x, (t_w, t_h))
-            new_img = np.full(self.size + (3,), self.fill_color, dtype=x.dtype)
+            img = cv2.resize(img, (t_w, t_h))
+            new_img = np.full(self.size + (3,), self.fill_color, dtype=img.dtype)
 
             # Insert image
-            new_img[d_h1:self.size[0]-d_h2, d_w1:self.size[1]-d_w2, :] = x
+            new_img[d_h1:self.size[0]-d_h2, d_w1:self.size[1]-d_w2, :] = img
 
             return new_img, lambda x, y: (int(x*t_w/w + (self.size[1]-t_w)/2),
                                           int(y*t_h/h + (self.size[0]-t_h)/2))
         else:
-            x = cv2.resize(x, (t_w, t_h))
-            return x, lambda x, y: (int(x*t_w/w), int(y*t_h/h))
+            new_img = cv2.resize(img, (t_w, t_h))
+            return new_img, lambda x, y: (int(x*t_w/w), int(y*t_h/h))
 
 
 class HFlip(Transform):
@@ -81,10 +82,9 @@ class HFlip(Transform):
         super().__init__()
 
     def __call__(self, img):
-        h, w, c = img.shape
-        new_img = np.zeros((h, w, c), dtype=img.dtype)
-        new_img[:] = img[:, ::-1, :]
-        return new_img, lambda x, y : ((w-1) - x, y)
+        w = img.shape[1]
+        new_img = img[:, ::-1, :]
+        return new_img, lambda x, y: ((w-1) - x, y)
 
 
 class DeltaBright(Transform):
@@ -94,18 +94,15 @@ class DeltaBright(Transform):
     def __init__(self):
         super().__init__()
 
-    def __call__(self, x):
-        h, w, c = x.shape
-        new_img = np.zeros((h, w, c), dtype=np.float32)
-        new_img[:] = x
-        new_img = cv2.cvtColor(new_img, cv2.COLOR_BGR2HSV)
+    def __call__(self, img):
+        dtype = img.dtype
+        new_img = cv2.cvtColor(img.astype('float32'), cv2.COLOR_BGR2HSV)
         new_img[..., 2] = np.clip(
             new_img[..., 2] * np.random.uniform(low=0.5, high=1.5),
             0,
             255
         )
-        new_img = cv2.cvtColor(new_img, cv2.COLOR_HSV2BGR)
-        new_img = new_img.astype('uint8')
+        new_img = cv2.cvtColor(new_img, cv2.COLOR_HSV2BGR).astype(dtype)
         return new_img, lambda x, y: (x, y)
 
 
